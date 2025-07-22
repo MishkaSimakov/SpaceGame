e3f75b7aa69455d03579ea817fd1880a40ed91e5
MishkaSimakov <msimakov661@gmail.com>
2025-07-22 16:24:44 +0300

    draw card phase simple implementation

--- common/Player.ts
+++ common/Player.ts
@@ -4,48 +4,6 @@ import {Event} from "./events/Event";
 import {Options} from "./PlainToClass";
 import {OtherPlayer} from "./GameForPlayerDTO";
 
-function isObject(object) {
-    return object != null && typeof object === 'object';
-}
-
-function deepEqual(object1: Object, object2: Object, exclude: string[] = []): boolean {
-    const keys1 = Object.keys(object1);
-    const keys2 = Object.keys(object2);
-
-    if (keys1.length !== keys2.length)
-        return false;
-
-    for (const key of keys1) {
-        if (exclude.indexOf(key) !== -1)
-            continue;
-
-        const val1 = object1[key];
-        const val2 = object2[key];
-        const areObjects = isObject(val1) && isObject(val2);
-        if (
-            areObjects && !deepEqual(val1, val2, exclude) ||
-            !areObjects && val1 !== val2
-        )
-            return false;
-    }
-
-    return true;
-}
-
-function arrayCompare<T>(arr1: Array<T>, arr2: Array<T>) {
-    if (arr1.length !== arr2.length)
-        return false;
-
-    for (const idx_1 of arr1.keys())
-        for (const idx_2 of arr2.keys())
-            if (deepEqual(arr1[idx_1], arr2[idx_2], ['x', 'y', 'rotation'])) {
-                arr2.splice(idx_2, 1);
-                break;
-            }
-
-    return !arr2.length;
-}
-
 export type PlayerId = number;
 
 export default class Player {
@@ -67,13 +25,6 @@ export default class Player {
     protected lose: boolean = false;
 
     constructor() {
-    }
-
-    canBeTurnedInto(newHand: (Module | Event)[], newSpaceship: Spaceship): boolean {
-        let currentCards = [...this.spaceship.modules, ...this.hand];
-        let changedCards = [...newSpaceship.modules, ...newHand];
-
-        return arrayCompare(currentCards, changedCards);
     }
 
     collectEnergy() {
