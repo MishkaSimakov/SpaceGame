4e5af976c7d0c05d7c9f78bd72900591ec40aee0
MishkaSimakov <msimakov661@gmail.com>
2025-07-31 17:07:33 +0300

    changed randomizer to actions, added information requests

---
+++ server/src/game/sagas/components/ShowCards.ts
@@ -0,0 +1,25 @@
+import Module from "@common/modules/Module";
+import {Event} from "@common/events/Event";
+import Player from "@common/Player";
+import {request} from "./Request";
+import {showCardsInfo, showCardsToPlayersRequest, showCardsToPlayersResponse} from "@common/actions/Main";
+import {put, select} from "../../Effects";
+
+export function* showCards(player: Player, cards: (Module | Event)[], showToOthers: boolean) {
+    const state = yield* select();
+
+    if (showToOthers) {
+        for (const otherPlayer of state.players) {
+            if (otherPlayer.id === player.id) {
+                continue;
+            }
+
+            yield* put(showCardsInfo(otherPlayer, cards));
+        }
+    }
+
+    yield* request(
+        showCardsToPlayersRequest(player, cards),
+        showCardsToPlayersResponse
+    );
+}
